d42d1ead4e1cf94149943a4cf8ea79f0dd96eaef
Jordan Suber <jordansuber@gmail.com>
2026-02-04 13:52:15 -0500

    Fixes diagram error

--- src/types/railroad-diagrams.d.ts
+++ src/types/railroad-diagrams.d.ts
@@ -1,3 +1,18 @@
+declare module "@prantlf/railroad-diagrams" {
+  export const Diagram: any;
+  export const Sequence: any;
+  export const Choice: any;
+  export const Optional: any;
+  export const OneOrMore: any;
+  export const ZeroOrMore: any;
+  export const Terminal: any;
+  export const NonTerminal: any;
+  export const Stack: any;
+  export const Comment: any;
+  export const Skip: any;
+  export const Group: any;
+}
+
 declare module "@prantlf/railroad-diagrams/lib/index.mjs" {
   export const Diagram: any;
   export const Sequence: any;
@@ -9,4 +24,6 @@ declare module "@prantlf/railroad-diagrams/lib/index.mjs" {
   export const NonTerminal: any;
   export const Stack: any;
   export const Comment: any;
+  export const Skip: any;
+  export const Group: any;
 }
